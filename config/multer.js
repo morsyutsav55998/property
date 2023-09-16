99b7cc05e06aed23cab279b8bac5fdf80d5b8937
morsyutsav55998 <morsyutsav@gmail.com>
2023-09-16 10:22:07 +0530

    utsav

--- config/multer.js
+++ config/multer.js
@@ -1,18 +1,29 @@
-const multer = require('multer')
+const multer=require('multer');
 const path = require('path')
-const imagePath = './images'
+const sharp = require('sharp');
+const avtarpath = '/images'
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null,path.join(__dirname,'..',imagePath))
+        cb(null,path.join(__dirname,'..',avtarpath))
     },
     filename:(req,file,cb)=>{
         var extension = path.extname(file.originalname)
         cb(null,file.fieldname+'-'+Date.now()+extension)
     } 
 })
+const upload= multer({
+    storage:storage,
+    fileFilter: function (req, file, cb) {
+        const filetypes = /jpeg|jpg|png/;
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
-const upload = multer({
-    storage: storage,
-})
+        if (mimetype && extname) {
+            return cb(null, true);
+        }
 
-module.exports = upload
+        cb("Error: File upload only supports the following filetypes - " + filetypes);
+    }
+});
+
+module.exports=upload;
